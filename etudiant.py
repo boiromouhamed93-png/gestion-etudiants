94b91b61aa2c12e5462edbe452cfe5885e59770e
boiromouhamed93-png <boiromouhamed93@gmail.com>
2026-09-12 20:07:01 +0000

    la fonction ajouter

--- etudiant.py
+++ etudiant.py
@@ -1,7 +1,12 @@
 print("mon premier projet que je doit presente sur github ")
 class Gestion :
     def __init__( self,nom,age):
-        self.mom=nom
+        self.nom=nom
         self.age=age
     def afficher():
         print("je suis un etudiant")
+def ajouter ():
+    nom= input("donner le nom de l'etudiant")
+    age=input("donner l'age de l'age")
+    e=gestion(nom,age)
+    return e
