008ab4ee8f26e5cf4aabb82502c109d6636cfac5
mouhamad <boiromouhamed93@gmail.com>
2026-09-14 09:26:23 +0000

    la fonction chercher et delacration de liste

--- etudiant.py
+++ etudiant.py
@@ -3,10 +3,30 @@ class Gestion :
     def __init__( self,nom,age):
         self.nom=nom
         self.age=age
-    def afficher():
+    def afficher(self):
+        print("nom : ",self.nom,"\nage: ",self.age)
         print("je suis un etudiant")
-def ajouter ():
+def ajouter():
     nom= input("donner le nom de l'etudiant")
     age=input("donner l'age de l'age")
     e=Gestion(nom,age)
     return e
+def chercher(liste):
+    nom_rhc=input("donner le nom de l'etudiant que vous rechercher")
+    for i in range(len(liste)):
+        if nom_rhc==liste[i].nom:
+            print("trouver\n infos")
+            liste[i].afficher()
+            return
+    print(nom_rhc,"nest pas dans la liste")
+liste=[]
+for i in range(1):
+    liste.append(ajouter())
+for i in range(1):
+    liste[i].afficher()
+chercher(liste)
+
+
+
+
+            
